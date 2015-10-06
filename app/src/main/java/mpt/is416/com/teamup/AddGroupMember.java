bdd53b6a818463682b89d15e7844c6519a464a3d
cheukolate <cychu.2012@sis.smu.edu.sg>
2015-10-06 18:04:38 +0800

    Add Participants with Gallery

--- app/src/main/java/mpt/is416/com/teamup/AddGroupMember.java
+++ app/src/main/java/mpt/is416/com/teamup/AddGroupMember.java
@@ -1,16 +1,20 @@
 package mpt.is416.com.teamup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AddGroupMember extends AppCompatActivity {
-
+    ImageView contactImgView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +31,42 @@ public class AddGroupMember extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        contactImgView = (ImageView) findViewById(R.id.imgViewContactImage);
+        contactImgView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent.createChooser(intent, "Select Contact Image"), 1);
+
+            }
+
+        });
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_add_group_member, container, false);
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getActivity(),"sssss",Toast.LENGTH_SHORT).show();
+                goToSecondActivity();
+
+            }
+        });
+
+
+
+        return v;
+    }
+
+    private void goToSecondActivity() {
+
+        Intent intent = new Intent(this,AddGroupMember.class);
+
+        startActivity(intent);
+
     }
 
     public boolean onOptionsItemSelected(MenuItem menuItem) {
