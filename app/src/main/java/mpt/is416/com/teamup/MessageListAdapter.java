0d56a22d798955d89ecc3255bf9e0f59a5de4f3d
FENG XIN <xin.feng.2012@sis.smu.edu.sg>
2015-11-19 11:50:54 +0800

    Modify Incoming Msg Verification

--- app/src/main/java/mpt/is416/com/teamup/MessageListAdapter.java
+++ app/src/main/java/mpt/is416/com/teamup/MessageListAdapter.java
@@ -22,19 +22,13 @@ public class MessageListAdapter extends BaseAdapter {
     public Context context;
     private List<Pair<Message, Integer>> messages;
 
-    //    Date date = new Date();
-//    Message message1 = new Message("m1", "cr1", new Timestamp(date.getTime()), "message1");
-    //Message message2 = new Message("m2", "cr2", new Timestamp(date.getTime()), "message2");
     MessageListAdapter(Context c){
         this.context = c;
         messages = new ArrayList<Pair<Message, Integer>>();
-//        messages.add(new Pair(message1,1));
-        //messages.add(new Pair(message2,1));
     }
 
     public void addMessage(Message message, int direction) {
         messages.add(new Pair<Message, Integer>(message, direction));
-        //notifyDataSetChanged();
     }
 
     public void clearArrayList(){
@@ -67,12 +61,14 @@ public class MessageListAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         int direction = getItemViewType(position);
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
+        TextView messageTime, textMsg, senderName = null;
         if(convertView == null){
             if(direction == 0){
                 convertView = inflater.inflate(R.layout.single_chat_message, parent, false);
                 convertView.findViewById(R.id.content_with_background)
                         .setBackgroundResource(R.drawable.out_message_bg);
+
+                senderName = (TextView)convertView.findViewById(R.id.senderName);
             }else if(direction == 1){
                 convertView = inflater.inflate(R.layout.right_single_chat_message, parent, false);
                 convertView.findViewById(R.id.content_with_background)
@@ -80,33 +76,17 @@ public class MessageListAdapter extends BaseAdapter {
             }
         }
 
-        //LinearLayout linearLayoutOuter = (LinearLayout)convertView.findViewById(R.id.message_content);
-        //LinearLayout linearLayoutInner = (LinearLayout)convertView.findViewById(R.id.content_with_background);
-        TextView messageTime = (TextView)convertView.findViewById(R.id.textInfo);
-        TextView textMsg = (TextView)convertView.findViewById(R.id.textMessage);
+        messageTime = (TextView)convertView.findViewById(R.id.textInfo);
+        textMsg = (TextView)convertView.findViewById(R.id.textMessage);
 
         Message message = messages.get(position).first;
-        //LinearLayout.LayoutParams paramsOuter = (LinearLayout.LayoutParams)linearLayoutOuter.getLayoutParams();
-        // set the message to left if incoming, right if outgoing
-
-        // Incoming when direction is 0
-        /*if(direction == 0){
-            ((RelativeLayout)convertView.findViewById(R.id.single_message)).setGravity(Gravity.START);
-            ((LinearLayout)convertView.findViewById(R.id.content_with_background)).setBackgroundResource(R.drawable.out_message_bg);
-            //paramsOuter.gravity = RelativeLayout.ALIGN_PARENT_LEFT;
-            //linearLayoutInner.setBackgroundResource(R.drawable.out_message_bg);
-        }else if(direction == 1){
-        // Outgoing when direction is 1
-            ((RelativeLayout)convertView.findViewById(R.id.single_message)).setGravity(Gravity.END);
-            //((RelativeLayout)convertView.findViewById(R.id.single_message))
-            ((LinearLayout)convertView.findViewById(R.id.content_with_background)).setBackgroundResource(R.drawable.in_message_bg);
-            //paramsOuter.gravity = RelativeLayout.ALIGN_PARENT_RIGHT;
-            //linearLayoutInner.setBackgroundResource(R.drawable.in_message_bg);
-        }*/
-
-        //linearLayoutOuter.setLayoutParams(paramsOuter);
         textMsg.setText(message.getTextMsg());
         messageTime.setText(message.getTimeStamp().toString());
+        if(message.getSenderName()!=null){
+            if(senderName != null){
+                senderName.setText(message.getSenderName());
+            }
+        }
 
         return convertView;
     }
